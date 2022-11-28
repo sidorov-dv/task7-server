d9ad3e8ed4ba9684e6f943d8bbdaad512020afeb
Dmitry Sidorov <demsys14@gmail.com>
2022-11-28 21:52:00 +0300

    server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log("User Connected");
+
+  socket.on("joinRoom", (roomCode) => {
+    console.log(`User joined the room ${roomCode}`);
+    socket.join(roomCode);
+  });
+
+  socket.on("play", ({ id, roomCode }) => {
+    console.log(`Play at ${id} to ${roomCode}`);
+    socket.broadcast.to(roomCode).emit("updateGame", id);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
+});
+
+server.listen(5555, () =>
+  console.log("server starts on http://localhost:5555")
+);
